7aa7344274ba6798ee15a25129d52c0e37ef399d
Yarin Nissan Avraham <yarinnissan1994@gmail.com>
2023-06-03 23:23:21 +0300

    refactoring

--- src/Main.java
+++ src/Main.java
@@ -7,33 +7,33 @@ public class Main {
     public static void main(String[] args) {
 
             //Scanner scanner = new Scanner(System.in);
-            System.out.println("variables:");
-            PrimitiveTypes primitive = new PrimitiveTypes();
-            primitive.run();
+//            System.out.println("variables:");
+//            PrimitiveTypes primitive = new PrimitiveTypes();
+//            primitive.run();
 
-            System.out.println("\nclasses:");
-            ClassBasics basics = new ClassBasics();
-            //String userInput = scanner.nextLine();
-            System.out.println(basics.inputChaining("test"));
-
-            ClassConstructors singleArg = new ClassConstructors(1);
-            ClassConstructors doubleArg = new ClassConstructors(2, 3);
-
-            System.out.println(ClassBasics.numMult(3));
-
-            AnimalFactory animalFactory = new AnimalFactory();
-
-            Animal dog = animalFactory.createAnimal("dog");
-            dog.makeSound();
-
-            Animal cat = animalFactory.createAnimal("cat");
-            cat.makeSound();
-
-            System.out.println("\nencapsulation (properties):");
-            ClassEncapsulation encapsulation = new ClassEncapsulation();
-            encapsulation.setName("yarin");
-            encapsulation.setAge(28);
-            System.out.println(encapsulation.getName());
-            System.out.println(encapsulation.getAge());
+//            System.out.println("\nclasses:");
+//            ClassBasics basics = new ClassBasics();
+//            //String userInput = scanner.nextLine();
+//            System.out.println(basics.inputChaining("test"));
+//
+//            ClassConstructors singleArg = new ClassConstructors(1);
+//            ClassConstructors doubleArg = new ClassConstructors(2, 3);
+//
+//            System.out.println(ClassBasics.numMult(3));
+//
+//            AnimalFactory animalFactory = new AnimalFactory();
+//
+//            Animal dog = animalFactory.createAnimal("dog");
+//            dog.makeSound();
+//
+//            Animal cat = animalFactory.createAnimal("cat");
+//            cat.makeSound();
+//
+//            System.out.println("\nencapsulation (properties):");
+//            ClassEncapsulation encapsulation = new ClassEncapsulation();
+//            encapsulation.setName("yarin");
+//            encapsulation.setAge(28);
+//            System.out.println(encapsulation.getName());
+//            System.out.println(encapsulation.getAge());
         }
     }
